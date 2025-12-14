9f0d44a54fe7b732893ab560b23ff223f13b45e1
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2025-12-14 19:03:30 +0100

    Basic Julia wrapper for vecmap unsupervised mode

---
+++ README.md
@@ -0,0 +1,3 @@
+# VecMap.jl
+
+This package is a simple wrapper for the Python [vecmap](https://github.com/artetxem/vecmap) implementation. This wrapper does not provide the full range of vecmap yet, but is functional.
---
+++ src/VecMap.jl
@@ -0,0 +1,26 @@
+module VecMap
+
+export unsupervised
+
+const Constraint = Union{"unconstrained", "orthogonal", Nothing}
+const Precision = Union{"fp16", "fp32", "fp64", Nothing}
+const Seed = Union{Integer, Nothing}
+const Threshold = Union{Float64, Nothing}
+const CSLS = Union{Integer, Nothing}
+
+function unsupervised(trg_in::String, trg_out::String, src_in::String, src_out::String, constraints::Constraint=nothing, precision::Precision=nothing, encoding::String="", cuda::Bool=false, whiten::Bool=false, batch_size::Integer=-1, seed::Seed=nothing, threshold::Threshold=nothing, csls::CSLS=nothing, verbose::Bool=false)
+  flags = []
+  if cuda push!(flags, "--cuda") end
+  if whiten push!(flags, "--whiten") end
+  if verbose push!(flags, "--verbose") end
+  if !isnothing(constraints) push!(flags, "--$(constraints)") end
+  if !isnothing(precision) push!(flags, "--precision $(precision)") end
+  if !isnothing(threshold) push!(flags, "--threshold $(threshold)") end
+  if batch_size > 0 push!(flags, "--batch_size $(batch_size)") end
+  if encoding !== "" push!(flags, "--encoding $(encoding)") end
+  if !isnothing(seed) push!(flags, "--seed $(seed)") end
+  if !isnothing(csls) push!(flags, "--csls $(csls)") end
+  run("python3 python/map_embeddings.py --unsupervised $(src_in) $(trg_in) $(src_out) $(trg_out) $(join(flags, " "))"; wait=true)
+end
+
+end
